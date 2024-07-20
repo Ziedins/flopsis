ff2c804152169bd0966e15015414da4254becf23
Reinis Ziedins <reinis.z@legalbet.com>
2024-07-20 14:31:52 +0300

    Add game tricycle asia

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 #[macro_use] extern crate rocket;
+use rocket_dyn_templates::{Template, context};
+use rocket::fs::{FileServer, relative};
+use rocket::response::content::RawHtml;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Esmu publisks!"
+fn index() -> RawHtml<&'static str> {
+    RawHtml("Esmu publisks</br> <a href='tricycleasia'>Trīsritenis pa āziju</a>")
 }
 
 #[get("/tricycleasia")]
-fn tricycleasia() -> &'static str {
-    "Te būs spēle"
+fn tricycleasia() -> Template {
+    Template::render("tricycleasia", context! {
+
+    })
 }
 
 #[launch]
@@ -15,4 +20,7 @@ fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
         .mount("/", routes![tricycleasia])
+        .mount("/out", FileServer::from(relative!("/out")))
+        .mount("/assets", FileServer::from(relative!("/out/tricycleasia/assets")))
+        .attach(Template::fairing())
 }
