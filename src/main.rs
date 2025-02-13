92888c7dc73b5ce18797e4edb3dd0d8578b33d66
Reinis Ziedins <reinis.z@legalbet.com>
2025-02-13 23:38:11 +0200

    Add a library with server logic, replace routes with single health_check endpoint, add tests dir

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,6 @@
-use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or(" Liisa");
-    format!("Hello {}!", name)
-}
+use flopsis::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/name/{name}", web::get().to(greet))
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    run().await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,6 @@
+use flopsis::main;
+
+#[test]
+fn dummy_test() {
+    main();
+}
