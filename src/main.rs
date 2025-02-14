29e3c05259b215b4946ad44d0af1390ede8bce46
Reinis Ziedins <reinis.z@legalbet.com>
2025-02-14 22:08:09 +0200

    Added app independant health_check test that starts and binds to free port in the backroung with tokio spawn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use flopsis::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind to 8080 port");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,28 @@
-use flopsis::main;
+use flopsis::run;
+use std::net::TcpListener;
+#[tokio::test]
+async fn health_check_works() {
+    let address: String = spawn_app();
 
-#[test]
-fn dummy_test() {
-    main();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
