05db8152670de560996402a87fea76d1a4ad5227
Ziedins <ziedinslu@gmail.com>
2025-03-16 18:04:06 +0200

    Add ability to enable/disable testing with log output

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,30 @@
 use flopsis::startup::run;
 use std::net::TcpListener;
-use actix_web::web::get;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use flopsis::configuration::{get_configuration, DatabaseSettings};
+use flopsis::telemetry::{get_subscriber, init_subscriber};
+use once_cell::sync::Lazy;
 
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
 }
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
